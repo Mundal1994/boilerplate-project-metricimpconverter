f25a6907f1f25b46c4e62a6e7f9b426998860d19
Mundal1994 <manga_flip@hotmail.com>
2024-07-17 21:36:27 +0300

    implemented getReturnUnit function

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -13,9 +13,22 @@ function ConvertHandler() {
   };
   
   this.getReturnUnit = function(initUnit) {
-    let result;
-    
-    return result;
+    switch (initUnit) {
+      case 'mi':
+        return 'km';
+      case 'km':
+        return 'mi';
+      case 'gal':
+        return 'L';
+      case 'L':
+        return 'gal';
+      case 'lbs':
+        return 'kg';
+      case 'kg':
+        return 'lbs';
+      default:
+        return 'error';
+    }
   };
 
   this.spellOutUnit = function(unit) {
@@ -28,7 +41,25 @@ function ConvertHandler() {
     const galToL = 3.78541;
     const lbsToKg = 0.453592;
     const miToKm = 1.60934;
+
+    switch(this.getReturnUnit(initUnit)) {
+      case 'mi':
+        // calculation
+        break;
+      case 'km':
+        console.log("return unit km");
+        break;
+      case 'gal':
+      case 'L':
+      case 'lbs':
+      case 'kg':
+        break;
+      default:
+        break;
+    }
     let result;
+    console.log("initNum: ", initNum, " initUnit: ", initUnit);
+
     
     return result;
   };
--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -5,5 +5,7 @@ const ConvertHandler = require('../controllers/convertHandler.js');
 let convertHandler = new ConvertHandler();
 
 suite('Unit Tests', function(){
-
+    test('#whole number', function(){
+        console.log("convertHandler: ", convertHandler.convert(3.1, 'mi'));
+    });
 });
