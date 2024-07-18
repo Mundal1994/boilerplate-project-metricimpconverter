52c558adb94b697c06795197f5724ac0e3d976d7
Mundal1994 <manga_flip@hotmail.com>
2024-07-19 00:09:01 +0300

    started creating the routes

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,51 @@
 'use strict';
 
 const expect = require('chai').expect;
+const { Router } = require('express');
 const ConvertHandler = require('../controllers/convertHandler.js');
 
 module.exports = function (app) {
   
-  let convertHandler = new ConvertHandler();
+  app.route('/api/convert').get((req, res) => {
+    const convertHandler = new ConvertHandler();
+    const input = req.query.input;
+    console.log("api/convert called with input: ", input)
 
+    let initNum;
+    let initUnit;
+    let error = '';
+    
+    try {
+      initNum = convertHandler.getNum(input);
+    } catch(e) {
+      error = e;
+    }
+
+    try {
+      initUnit = convertHandler.getUnit(input);
+    } catch(e) {
+      if (!error.length) {
+        error = e;
+      } else {
+        error += ' and unit';
+      }
+    }
+ 
+    if (!error.length) {
+      const returnNum = convertHandler.convert(initNum, initUnit);
+      const returnUnit = convertHandler.getReturnUnit(initUnit);
+      const string = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
+      console.log("returnNum: ", returnNum, 'returnUnit',returnUnit, 'string:',string);
+      
+      let result = ({
+        'initNum': initNum, 
+        'initUnit': initUnit, 
+        'returnNum': returnNum, 
+        'returnUnit': returnUnit, 
+        'string': string});
+      res.json(result);
+    } else {
+      res.json(error);
+    }
+  });
 };
