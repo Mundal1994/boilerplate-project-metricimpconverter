b728ecfbb870a0b97388bd55baffe65715ae9a4c
Mundal1994 <manga_flip@hotmail.com>
2024-07-19 14:40:12 +0300

    added additional unit tests in 1_unit_tests

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -7,7 +7,8 @@ let convertHandler = new ConvertHandler();
 suite('Unit Tests', function(){
     suite('Basic values', function() {
         // whole number
-        test('#whole number', function(){
+        //#1
+        test('#whole number 4gal', function(){
             let result1 = {
                 initNum: 4,
                 initUnit: 'gal',
@@ -20,7 +21,9 @@ suite('Unit Tests', function(){
             assert.strictEqual(result1['returnNum'], convertHandler.convert(4, 'gal'));
             assert.strictEqual(result1['returnUnit'], convertHandler.getReturnUnit('gal'));
             assert.strictEqual(result1['string'], convertHandler.getString(4, 'gal', result1['returnNum'], result1['returnUnit']));
-
+        });
+        //#2
+        test('#whole number 6mi', function() {
             let result2 = {
                 initNum: 6,
                 initUnit: 'mi',
@@ -35,7 +38,8 @@ suite('Unit Tests', function(){
             assert.strictEqual(result2['string'], convertHandler.getString(6, 'mi', result2['returnNum'], result2['returnUnit']));
         });
         // decimal number
-        test('#decimal number', function(){
+        //#3
+        test('#decimal number 3.1mi', function(){
             let result1 = {
                 initNum: 3.1,
                 initUnit: 'mi',
@@ -48,7 +52,9 @@ suite('Unit Tests', function(){
             assert.strictEqual(result1['returnNum'], convertHandler.convert(3.1, 'mi'));
             assert.strictEqual(result1['returnUnit'], convertHandler.getReturnUnit('mi'));
             assert.strictEqual(result1['string'], convertHandler.getString(3.1, 'mi', result1['returnNum'], result1['returnUnit']));
-
+        });
+        //#4
+        test('#decimal number 6.43736km', function() {
             let result2 = {
                 initNum: 6.43736,
                 initUnit: 'km',
@@ -63,7 +69,8 @@ suite('Unit Tests', function(){
             assert.strictEqual(result2['string'], convertHandler.getString(6.43736, 'km', result2['returnNum'], result2['returnUnit']));
         });
         // fractional input
-        test('#fractional input', function(){
+        //#5
+        test('#fractional input 1/2km', function(){
             let result1 = {
                 initNum: 0.5,
                 initUnit: 'km',
@@ -76,7 +83,9 @@ suite('Unit Tests', function(){
             assert.strictEqual(result1['returnNum'], convertHandler.convert(0.5, 'km'));
             assert.strictEqual(result1['returnUnit'], convertHandler.getReturnUnit('km'));
             assert.strictEqual(result1['string'], convertHandler.getString(0.5, 'km', result1['returnNum'], result1['returnUnit']));
-
+        });
+        //#6
+        test('#fractional input 1/5kg', function() {
             let result2 = {
                 initNum: 0.2,
                 initUnit: 'kg',
@@ -91,7 +100,8 @@ suite('Unit Tests', function(){
             assert.strictEqual(result2['string'], convertHandler.getString(0.2, 'kg', result2['returnNum'], result2['returnUnit']));
         });
         // fractional input with a decimal
-        test('#fractional input with a decimal', function(){
+        //#7
+        test('#fractional input with a decimal 10.5/5lbs', function(){
             let result1 = {
                 initNum: 2.1,
                 initUnit: 'lbs',
@@ -104,7 +114,9 @@ suite('Unit Tests', function(){
             assert.strictEqual(result1['returnNum'], convertHandler.convert(2.1, 'lbs'));
             assert.strictEqual(result1['returnUnit'], convertHandler.getReturnUnit('lbs'));
             assert.strictEqual(result1['string'], convertHandler.getString(2.1, 'lbs', result1['returnNum'], result1['returnUnit']));
-
+        });
+        //#8
+        test('#fractional input with a decimal 0.5/2L', function() {
             let result2 = {
                 initNum: 0.25,
                 initUnit: 'L',
@@ -122,7 +134,8 @@ suite('Unit Tests', function(){
     
     suite('Edge cases', function() {
         // default to numerical value of 1 when no numerical input is provided
-        test('#default to numerical value of 1 if no input', function(){
+        //#9
+        test('#default to numerical value of 1 if no input', function() {
             let result1 = {
                 initNum: 1,
                 initUnit: 'kg',
@@ -135,7 +148,9 @@ suite('Unit Tests', function(){
             assert.strictEqual(result1['returnNum'], convertHandler.convert(1, 'kg'));
             assert.strictEqual(result1['returnUnit'], convertHandler.getReturnUnit('kg'));
             assert.strictEqual(result1['string'], convertHandler.getString(1, 'kg', result1['returnNum'], result1['returnUnit']));
-
+        });
+        //#10
+        test('#default to numerical value of 1 if no input test 2', function() {
             let result2 = {
                 initNum: 1,
                 initUnit: 'gal',
